b5458ff9c2034912ccd309a8e31ea3ecc8ba2074
PythonHacker24 <adityapatil24680@gmail.com>
2024-09-15 11:33:47 +0530

    updated install script

--- install.sh
+++ install.sh
@@ -96,53 +96,5 @@ git clone https://github.com/PythonHacker24/awesomevim.git "$NVIM_DIR" || { echo
 
 # Source packer.lua and run PackerSync
 echo "Running PackerSync..."
-#nvim -c 'luafile ~/.config/nvim/custom/packer.lua' -c 'PackerSync' -c ""
 nvim -c 'luafile ~/.config/nvim/custom/packer.lua' -c 'PackerSync' -c 'autocmd User PackerComplete quitall'
 echo "Plugins installed successfully!"
-
-# Configuring AwesomeVim
-echo "Configuring AwesomeVim..."
-
-
-# Update init.lua to use Awesome theme
-echo 'require("startup").setup({theme = "awesome"})' >> "$HOME/.config/nvim/init.lua"
-
-# Open Neovim to complete the setup
-echo "Opening Neovim..."
-
-# Source and destination file paths
-SOURCE_FILE="$NVIM_DIR/custom/awesome.lua"
-DEST_FILE="lua/startup/themes/awesome.lua"
-
-# Function to copy the file
-copy_file() {
-    local dest_dir=$1
-    mkdir -p "$dest_dir" || { echo "Failed to create destination directory"; exit 1; }
-    cp "$SOURCE_FILE" "$dest_dir/$DEST_FILE" || { echo "Failed to copy file"; exit 1; }
-}
-
-# Detect the OS and determine destination path for copying files
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    # Linux detected
-    echo "Detected Linux"
-    dest_dir="$HOME/.local/share/nvim/site/pack/packer/start/startup.nvim"
-    copy_file "$dest_dir" || echo { echo "Failed to copy Awesome Vim ... exiting"; exit 1}
-
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    # macOS detected
-    echo "Detected macOS"
-    dest_dir="$HOME/.local/share/nvim/site/pack/packer/start/startup.nvim"
-    copy_file "$dest_dir"
-
-elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
-    # Windows detected (Cygwin/MSYS/WSL)
-    echo "Detected Windows"
-    dest_dir="/mnt/c/Users/$(whoami)/AppData/Local/nvim/site/pack/packer/start/startup.nvim"
-    copy_file "$dest_dir"
-
-else
-    echo "Unsupported OS: $OSTYPE"
-    exit 1
-fi
-
-nvim
