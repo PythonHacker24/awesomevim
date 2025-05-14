64c6aa929031f76ae378581d31a51b43d959e3a9
PythonHacker24 <adityapatil24680@gmail.com>
2025-05-14 09:31:04 +0530

    Fixed treesitter keymap

--- custom/remaps.lua
+++ custom/remaps.lua
@@ -22,9 +22,11 @@ vim.keymap.set('n', '<leader>m', vim.cmd.UndotreeToggle)
 
 vim.keymap.set("n", "K", require("hover").hover, { desc = "hover.nvim" })
 vim.keymap.set("n", "gK", require("hover").hover_select, { desc = "hover.nvim (select)" })
+
 vim.keymap.set("n", "<C-p>", function()
     require("hover").hover_switch("previous")
 end, { desc = "hover.nvim (previous source)" })
-vim.keymap.set("n", "<C-n>", function()
+
+vim.keymap.set("n", "<C-l>", function()
     require("hover").hover_switch("next")
 end, { desc = "hover.nvim (next source)" })
